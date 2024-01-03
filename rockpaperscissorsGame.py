5c9abead7ea652ccf0f0b8cb908bba30438050fa
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-02 23:00:38 -0500

    Update rockpaperscissorsGame.py
    Currently: Declared all variables, inputs for the number of games desired, set up a loop for the game where user input is inputted and checked and the computer makes a choice which is printed.
    
    To-Do: Figure out how to determine the winner, display current and final scores, and determine the winner from all games.

--- rockpaperscissorsGame.py
+++ rockpaperscissorsGame.py
@@ -1,1 +1,26 @@
+import random
 
+# Default values
+choices = {'R': "Rock", 'P': "Paper", 'S': "Scissors"}
+user_score = 0
+comp_score = 0
+
+# Input number of games to play
+total_games = int(input("Enter the number of games you want to play: "))
+
+# Game loop
+while comp_score + user_score < total_games:
+    # User input
+    user_choice = input("\nYour choice (R/P/S): ").upper()
+
+    # Check for valid input
+    if user_choice not in choices:
+        print("Invalid input. Please enter R, P, or S.")
+        continue
+
+    # Computer choice
+    comp_choice = random.choice(list(choices.keys()))
+
+    print("Computer's choice: ", choices[comp_choice])
+
+    
