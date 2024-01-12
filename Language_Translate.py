def5ca00ea8713e9463a7887361b6ed2289d3c82
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-12 00:48:14 -0500

    Update Language_Translate.py
    Added function to display all languages. Added user input for language to translate.

--- Language_Translate.py
+++ Language_Translate.py
@@ -20,3 +20,22 @@ class LanguageTranslator:
             "nl": "Dutch",
             "vi": "Vietnamese"
         }
+        
+    def display_language_options(self):
+        print("Code : Language")
+        for code, language in self.languages.items():
+            print(f"{code} => {language}")
+        print()
+
+    def get_user_language(self):
+        while True:
+            user_code = input(
+                "Please input the desired language code. To see the language code list, enter 'options':\n")
+
+            if user_code == "options":
+                self.display_language_options()
+            elif user_code in self.languages:
+                print(f"You have selected {self.languages[user_code]}")
+                return user_code
+            else:
+                print("It's not a valid language code!")
