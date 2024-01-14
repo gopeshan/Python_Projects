e56835eeb00f1596d4642a41eabb09aa8f5c40f7
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-14 01:41:27 -0500

    Update Language_Translate.py
    Added function to translate text. Added function to ask the user what to translate and recall functions accordingly.

--- Language_Translate.py
+++ Language_Translate.py
@@ -39,3 +39,30 @@ class LanguageTranslator:
                 return user_code
             else:
                 print("It's not a valid language code!")
+
+    def translate_text(self, text, dest_language):
+        translated = self.translator.translate(text, dest=dest_language)
+        return translated
+
+    def run_translator(self):
+        while True:
+            user_code = self.get_user_language()
+
+            text = input("\nWrite the text you want to translate:\nTo exit the program, write 'close'\n")
+
+            if text.lower() == "close":
+                print("\nHave a nice day!")
+                break
+
+            translated = self.translate_text(text, user_code)
+
+            print(f"\n{self.languages[user_code]} translation: {translated.text}")
+            print(f"Pronunciation: {translated.pronunciation}")
+
+            for code, language in self.languages.items():
+                if translated.src == code:
+                    print(f"Translated from: {language}")
+
+if __name__ == "__main__":
+    language_translator = LanguageTranslator()
+    language_translator.run_translator()
